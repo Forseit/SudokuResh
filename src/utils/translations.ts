16f6b4010cddacd8a56e65161809c2489cc257b5
Forseit <140819655+Forseit@users.noreply.github.com>
2025-01-15 09:27:06 +0700

    Update translations.ts

--- src/utils/translations.ts
+++ src/utils/translations.ts
@@ -13,8 +13,8 @@ export const translations: Translations = {
     print: "Print",
   },
   ru: {
-    title: "Ебатель Дмитрия",
-    keyboard: "Ебать?",
-    print: "Сделать отсос Дмитрию",
+    title: "Решатель Судоку",
+    keyboard: "Клавиатура?",
+    print: "Распечатать",
   },
 };
