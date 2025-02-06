bd4d8eb8f1afe1bf2ac4575abf9d261902483eeb
Forseit <140819655+Forseit@users.noreply.github.com>
2025-02-06 22:48:27 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -5,6 +7,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Reviews from "./pages/Reviews";
+import ReviewDetails from "./pages/ReviewDetails";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +20,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/reviews" element={<Reviews />} />
+          <Route path="/reviews/:id" element={<ReviewDetails />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
